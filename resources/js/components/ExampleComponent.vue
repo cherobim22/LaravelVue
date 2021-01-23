c7d8742cca5ef4b83445ee34c7115d08f35fcaa6
cherobim22 <lucascherobim@hotmail.com>
2021-01-22 21:23:21 -0300

    initial commit

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Component</div>
+
+                    <div class="card-body">
+
+                     <input type="text" v-model="teste" >
+                     {{teste}}
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+    export default {
+         data: () => {
+              return{
+                   teste: ''
+                }
+            },
+        mounted() {
+
+            console.log('Component mounted.')
+
+        }
+    }
+</script>
+<style scoped>
+input{
+    border: 1px solid black;
+}
+</style>
