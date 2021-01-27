da03ec533acb16b6a35ab6c45a612c97d3ac37f3
cherobim22 <lucascherobim@hotmail.com>
2021-01-26 23:35:44 -0300

    update components

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -10,8 +10,15 @@
                      <input type="text" v-model="teste" >
                      {{teste}}
 
+                    <span>hello</span>
+                    </div>
+                <div class="campanhas">
+                    <div v-for="poll in teste" :key="poll.id" class="polls">
+                        <span>{{poll}}</span>
 
                     </div>
+                </div>
+
                 </div>
             </div>
         </div>
@@ -20,15 +27,43 @@
 
 <script>
 import axios from 'axios'
+import PollComponents from './PollComponents'
 
     export default {
          data: () => {
               return{
-                   teste: ''
+                   teste: '',
+                   data:null,
+                   polls:[],
+                   testes:[],
+                   name:[]
                 }
             },
+            components:{
+                PollComponents
+            },
         mounted() {
+            axios.get('http://localhost:8000/polls').then((res)=>{
+                // console.log(res.data);
+                this.polls = res.data
 
+                console.log(this.polls);
+                var cont = 0
+                this.polls.forEach(element => {
+                    console.log(element.id);
+                    this.name = element.description;
+                    axios.get(`http://localhost:8000/polls/${element.id}`).then((res)=>{
+
+                        // console.log(res.data)
+                        this.testes[cont] =res.data
+                        cont=cont+1;
+                    });
+
+                });
+
+            }).then(()=>{
+                console.log(this.testes)
+            });
             console.log('Component mounted.')
 
         }
@@ -37,5 +72,15 @@ import axios from 'axios'
 <style scoped>
 input{
     border: 1px solid black;
+}
+
+.polls{
+    border: 1px solid red;
+    width: 100px;
+    height: 100px;
+}
+.campanhas{
+    display: flex;
+    justify-content: space-between;
 }
 </style>
---
+++ resources/js/components/PollComponents.vue
@@ -0,0 +1,20 @@
+<template>
+   <div>
+        <span style="display:none" id="poll_id">{{id}}</span>
+        <span>{{description}}</span>
+   </div>
+</template>
+
+<script>
+export default {
+    props:{
+        id: Number,
+        description: String,
+        action: Function,
+    }
+}
+</script>
+
+<style>
+
+</style>
